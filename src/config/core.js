2f9791127ae37cd53c9587160bdfd261b6bd78be
Nitin Tiwari <95606924+thedarknight01@users.noreply.github.com>
2024-04-14 15:52:31 +0530

    core.js

--- src/config/core.js
+++ src/config/core.js
@@ -9,7 +9,7 @@ import {
   } from "@google/generative-ai"
   
   const MODEL_NAME = "gemini-1.0-pro";
-  const API_KEY = "AIzaSyCyOpyNnyiErfMHf3RJBssLhgNJCdN09vo";
+  const API_KEY = "";
   
   async function runChat(prompt) {
     const genAI = new GoogleGenerativeAI(API_KEY);
